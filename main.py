e3ebdd26e0070dc666d3678df9fae7becc42d995
Okan <okan.dalan11@gmail.com>
2022-06-15 14:53:58 +0300

    extraction for gitti gidiyor implemented

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-import scraper
 import asyncio
 import aiohttp
+import scraper
 import time
 
 async def main():
     print(f"started at {time.strftime('%X')}")
-    urls = ["https://www.hepsiburada.com/dreame-v10-pro-dikey-kablosuz-sarjli-supurge-genpa-garantili-p-HBV0000188N7U",
-            "https://www.hepsiburada.com/arnica-merlin-pro-2-si-1-arada-850-watt-dikey-elektrikli-supurge-p-evarnmerlinpro", 
-            "https://www.hepsiburada.com/arnica-et13311-tria-pro-dik-elektrikli-supurge-p-hbv00000pjbwd"]
+    urls = ["https://www.gittigidiyor.com/giyim-aksesuar/fast-step-unisex-sneaker-ayakkabi-930xaf044_pdp_784061090"]
 
     headers = {
-        'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
-        'Referer':"https://www.google.com.tr",
-        'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        'Accept-Encoding' : 'gzip, deflate, br',
-        'Accept-Language': "en-US,en;q=0.5",
-        'country-code': "Turkey"
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
+        "Referer":"https://www.google.com.tr",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" : "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "country-code": "Turkey"
     }
     tasks = []
     async with aiohttp.ClientSession(headers=headers) as session:
         for url in urls:
             tasks.append(scraper.fetchData(session, url))
 
-        data = await asyncio.gather(*tasks) 
+        data = await asyncio.gather(*tasks)
+        print(data)
     print(f"finished at {time.strftime('%X')}")
 if __name__ == "__main__":
     asyncio.run(main())
--- scraper.py
+++ scraper.py
@@ -1,14 +1,15 @@
+from itertools import product
 from bs4 import BeautifulSoup
 import logging
-
+import cchardet
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
 
-file_handler = logging.FileHandler('scraper.log')
+file_handler = logging.FileHandler("scraper.log")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
@@ -21,29 +22,71 @@ async def fetchData(session, url):
     except Exception as e:
         logging.warning("Exception occured", exc_info=True)
     else:
-        return await extractDataHepsi(html)
+        return await extractDataGG(html)
+
 
 async def extractDataHepsi(html):
     soup = BeautifulSoup(html, "lxml")
-    productDetail = soup.find('div', class_ = 'productDetailContent')
 
-    productData = productDetail.find('div', class_ = "product-information col lg-5 sm-1")
+    productDetail = soup.find("div", class_ = "productDetailContent")
 
-    productTitle = productData.find('h1', itemprop = "name").text.strip()
+    productData = productDetail.find("div", class_ = "product-information col lg-5 sm-1")
 
-    productPriceRatings = productData.find('div', class_ = "product-price-and-ratings")
-    productOriginalPrice = productPriceRatings.find(id="originalPrice").text    # change , with . and strip whitespaces + TL
-    productOfferedPrice = productPriceRatings.find(id="offering-price")["content"]
-    productRatingsContainer = productPriceRatings.find('span', class_ = "ratings evaluate")
-    productRating = productRatingsContainer.find('span', itemprop = "ratingValue")["content"] # change , with .
-    productReviewCount = productRatingsContainer.find('span', itemprop = "reviewCount")["content"]
+    productTitle = productData.find("h1", itemprop = "name").text.strip()
+
+    productPriceAndRatings = productData.find("div", class_ = "product-price-and-ratings")
+    productOriginalPrice = productPriceAndRatings.find(id="originalPrice").text    # change , with . and strip whitespaces + TL
+    productOfferedPrice = productPriceAndRatings.find(id="offering-price")["content"]
+
+    productRatingsContainer = productPriceAndRatings.find("span", class_="ratings evaluate")
+    productRating = productRatingsContainer.find("span", itemprop="ratingValue")["content"] # change , with .
+    productReviewCount = productRatingsContainer.find("span", itemprop="reviewCount")["content"]
 
     productImageContainer = productDetail.find(id="productDetailsCarousel")
 
-    productImages = productImageContainer.find_all('a')
-    productMainImg = productImages[0].find('img')["src"]
-    productImgUrls = []
-    for i in productImages[1:]:
-        productImgUrls.append(i.find('img')['data-src'])
+    productMainImg = productImageContainer.find("a")
+    productMainUrl = productMainImg.find("img")["src"]
 
-    return {"Title": productTitle, "Price": productOfferedPrice, "Price Without Discount": productOriginalPrice, "Main Image": productMainImg, "Images": productImgUrls, "Rating Score": productRating, "Review Count": productReviewCount}    
+    productImgs = productMainImg.find_next_siblings("a")
+    productImgUrls = []
+    for img in productImgs:
+        productImgUrls.append(img.find("img")["data-src"])
+
+    return {"Title": productTitle, "Price": productOfferedPrice, 
+            "Price Without Discount": productOriginalPrice,
+            "Main Image": productMainUrl, "Images": productImgUrls, "Rating Score": productRating,
+            "Review Count": productReviewCount}
+
+
+async def extractDataGG(html):
+    soup = BeautifulSoup(html, "lxml")
+
+    productDetail = soup.find(id="gallery-title-price-sellerInformation")
+
+    productData = productDetail.find("div", class_="gg-w-13 gg-d-13 gg-t-24 gg-m-24 pr0 padding-none-m")
+
+    productTitleRatingContainer = productData.find(id="badgeTitleReviewBrand")
+    productTitle = productTitleRatingContainer.find(id="sp-title").text
+    productRating = productTitleRatingContainer.find(id="sp-reviewAverage").text
+    productReviewCount = productTitleRatingContainer.find(id="sp-reviewCommentCount").text
+
+    productPriceContainer = productDetail.find(id="sp-price-container")
+    productOriginalPrice = productPriceContainer.find(id="sp-price-highPrice").text.strip()
+    productOfferedPrice = productPriceContainer.find(id="sp-price-lowPrice") # olmama durumuna bakılacak
+
+    productImageContainer = productDetail.find(id="gallery")
+    
+    productMainImg = productImageContainer.find(id="big-photo")
+    productMainUrl = productMainImg["src"]
+
+    productImgsUl = productImageContainer.find("ul", class_="product-photos-ul") # normalde ul den sonra boşluk var ama çalışmıyor öyle
+    productImgs = productImgsUl.find_all("img")
+    productImgUrls = []
+    for img in productImgs:
+        productImgUrls.append(img["data-zoom-image"])    
+
+    return {"Title": productTitle, "Price": productOfferedPrice, 
+            "Price Without Discount": productOriginalPrice,
+            "Main Image": productMainUrl, "Images": productImgUrls, "Rating Score": productRating,
+            "Review Count": productReviewCount}
+    
